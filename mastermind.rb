076d1104123990b09ed646ccca7171d5e5d96c50
EvanOliver0 <ewoliver@protonmail.com>
2019-11-04 13:52:16 -0500

    Fill out game_over?, make_guess, evaluate_guess, count_matches methods

--- mastermind.rb
+++ mastermind.rb
@@ -12,11 +12,22 @@ class MastermindBoard
   end
 
   def game_over?
-
+    return true if @guesses.include? @code || @guesses.size >= @max_turns
   end
 
-  def make_guess
+  def make_guess(guess)
+    return nil unless valid_code? guess
 
+    if game_over?
+      puts "Can't make a new guess; the game is already over!"
+      return nil
+    end
+
+    response = evaluate_guess guess
+    @guesses.push guess
+    @responses.push response
+
+    return response
   end
 
   def set_code(code)
@@ -26,7 +37,7 @@ class MastermindBoard
     end
 
     valid = valid_code? code
-    @code = characters if valid
+    @code = code.split("") if valid
     return valid
   end
 
@@ -35,8 +46,31 @@ class MastermindBoard
   end
 
   private
-  def evaluate_guess
+  def count_matches(list1, list2)
+    if list1.size != list2.size
+      puts "Error in count_matches: array sizes do not match (#{list1.size} " \
+           "vs. #{list2.size})"
+      return 0
+    end
 
+    matches = 0
+    list1.size.times { |i| matches += 1 if list1[i] == list2[i] }
+
+    return matches
+  end
+
+  def evaluate_guess(guess)
+    guess = guess.split ""
+    total_pegs = 0
+    unique_code_colors = @code.uniq
+    unique_code_colors.each do |color|
+      total_pegs += [guess.count(color), @code.count(color)].min
+    end
+
+    red_pegs = count_matches(guess, @code)
+    white_pegs = total_pegs - red_pegs
+
+    return ("+" * red_pegs) + ("-" * white_pegs)
   end
 
   def valid_code?(code)
@@ -57,3 +91,12 @@ class MastermindBoard
     return valid
   end
 end
+
+board = MastermindBoard.new
+board.set_code "GBRK"
+puts board.make_guess "YYBB"
+puts board.make_guess "KRRY"
+puts board.make_guess "GKGY"
+puts board.make_guess "RRKY"
+puts board.make_guess "BRGK"
+puts board.make_guess "GBRK"
