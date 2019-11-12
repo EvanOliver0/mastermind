758c40af92b6c08aa1b9c4b94ddd91f17e5622c6
EvanOliver0 <ewoliver@protonmail.com>
2019-11-12 13:48:06 -0500

    Change from winner/loser to points; allow user to choose settings

--- mastermind.rb
+++ mastermind.rb
@@ -1,14 +1,13 @@
 class MastermindBoard
-  attr_reader :colors, :guesses, :max_guesses, :responses, :spaces, :victor
+  attr_reader :colors, :guesses, :max_guesses, :responses, :code_length
 
-  def initialize(colors = ["K", "W", "R", "G", "B", "Y"], spaces = 4,
+  def initialize(colors = ["K", "W", "R", "G", "B", "Y"], code_length = 4,
                  max_guesses = 12)
     @colors = colors
-    @spaces = spaces
+    @code_length = code_length
     @max_guesses = max_guesses
     @guesses = []
     @responses = []
-    @victor = nil
   end
 
   def game_over?
@@ -31,13 +30,17 @@ class MastermindBoard
     if @guesses.size > @max_guesses
       puts "How did you manage to make more guesses than allowed?"
       puts "Max guesses: #{@max_guesses}; total guesses: #{@guesses.size}"
-    elsif guess == @code
-      @victor = "codebreaker"
-    elsif @guesses.size == @max_guesses
-      @victor = "codemaker"
     end
 
     return response
+  end
+
+  def points
+    points = @guesses.size
+    if (@guesses.size == @max_guesses) && (!@guesses.include? @code)
+      points += 1
+    end
+    return points
   end
 
   def set_code(code)
@@ -53,18 +56,18 @@ class MastermindBoard
   end
 
   def to_s
-    representation = "*" * (@spaces * 2 + 3)
-    representation += "\n*" + " " * @spaces + "|"
-    representation += game_over? ? @code.join("") : ("?" * @spaces)
+    representation = "*" * (@code_length * 2 + 3)
+    representation += "\n*" + " " * @code_length + "|"
+    representation += game_over? ? @code.join("") : ("?" * @code_length)
     representation += "*"
     (@max_guesses - 1).downto(0) do |i|
       if i > (@guesses.size - 1)
-        representation += "\n*#{' ' * @spaces}|#{'-' * @spaces}*"
+        representation += "\n*#{' ' * @code_length}|#{'-' * @code_length}*"
       else
         representation += "\n*#{@responses[i]}|#{@guesses[i].join('')}*"
       end
     end
-    representation += "\n" + "*" * (@spaces * 2 + 3)
+    representation += "\n" + "*" * (@code_length * 2 + 3)
     return representation
   end
 
@@ -91,15 +94,15 @@ class MastermindBoard
 
     red_pegs = count_matches(guess, @code)
     white_pegs = total_pegs - red_pegs
-    blank = @spaces - total_pegs
+    blank = @code_length - total_pegs
 
     return (" " * blank) + ("-" * white_pegs) + ("+" * red_pegs)
   end
 
   def valid_code?(code)
     valid = true
-    if code.size != @spaces
-      puts "Please enter a #{@spaces}-character code."
+    if code.size != @code_length
+      puts "Please enter a #{@code_length}-character code."
       valid = false
     end
 
@@ -115,14 +118,21 @@ class MastermindBoard
 end
 
 class Player
-  attr_reader :score
-  def initialize(colors, spaces)
+  attr_reader :name, :score
+  def initialize(name = "Player", colors, code_length)
+    @name = name
     @score = 0
     @colors = colors
-    @code_length = spaces
+    @code_length = code_length
   end
 
   def choose_code
+    code = ""
+    @code_length.times do
+      choice = (rand * @colors.size).to_i
+      code += @colors[choice]
+    end
+    return code
   end
 
   def increase_score(points)
@@ -130,33 +140,28 @@ class Player
   end
 
   def make_guess(colors, length)
+    choose_code
   end
 end
 
 class ComputerPlayer < Player
-  def initialize(colors, spaces)
+  def initialize(name, colors, code_length)
     super
-    @possibilities = @colors
+    @possibilities = generate_combos(@colors, @code_length)
   end
 
   def choose_code
-    code = ""
-    @code_length.times do
-      choice = (rand * @possibilities.size).to_i
-      code += @possibilities[choice]
-    end
-    return code
+    return @possibilities.pop
   end
 
   def make_guess(past_guesses, past_responses)
     sleep 2
     if past_guesses.empty?
-      guess = choose_code()
+      guess = choose_code
     else
-      remove_impossible(past_guesses.last, past_responses.last)
       good = false
       until good
-        guess = choose_code()
+        guess = choose_code
         good = good_guess?(guess, past_guesses, past_responses)
       end
     end
@@ -165,6 +170,12 @@ class ComputerPlayer < Player
   end
 
   private
+  def generate_combos(colors, length)
+    combos = []
+    colors.repeated_permutation(length).each { |set| combos.push(set.join("")) }
+    return combos.shuffle
+  end
+
   def good_guess?(guess, past_guesses, past_responses)
     test_board = MastermindBoard.new(@colors, @code_length, past_guesses.size)
     test_board.set_code guess
@@ -174,11 +185,6 @@ class ComputerPlayer < Player
       return false unless test_board.make_guess(past_guess) == past_response
     end
     return true
-  end
-
-  def remove_impossible(guess, response)
-    pegs = response.length - response.count(" ")
-    @possibilities -= guess if pegs == 0
   end
 end
 
@@ -193,13 +199,103 @@ class HumanPlayer < Player
   end
 end
 
-board = MastermindBoard.new
-puts "Think you're a mastermind?"
+def get_code_length(min, max)
+  valid = false
+  until valid
+    length = get_integer("How long should the code be? (#{min} - #{max}) ",
+                         "Please enter a positive integer.")
+    if length < min || length > max
+      puts "Please enter a number between #{min} and #{max}."
+    else
+      valid = true
+    end
+  end
+  return length
+end
+
+def get_colors
+  puts "Colors are represented by single letters, like B or R."
+  valid, confirmed = false, false
+  until valid && confirmed
+    print "Enter the colors you'd like to use (no spaces/commas): "
+    raw_colors = gets.chomp
+    valid = only_alphabetic? raw_colors
+    if !valid
+      puts "Please enter only letters."
+      next
+    end
+    colors = raw_colors.upcase.split("").uniq
+    puts "So these are the colors we'll be using:"
+    puts colors.join ", "
+    print "Is that right? (Y/n) "
+    confirmation = gets.chomp.downcase
+    confirmed = confirmation != "n"
+  end
+  return colors
+end
+
+def get_integer(message, error_message = "Invalid input.")
+  valid  = false
+  until valid
+    print message
+    input = gets.chomp
+    valid = positive_integer? input
+    puts error_message unless valid
+  end
+  return Integer(input)
+end
+
+def get_max_guesses
+  return get_integer("How many guesses are allowed? ",
+                     "Please enter a positive integer.")
+end
+
+# Max code length allowable, given n colors, to limit processing time to <= 5 s
+def max_length_for(n)
+  max = case n
+    when 2 then 20
+    when 3 then 13
+    when 4 then 10
+    when 5 then 9
+    when 6 then 8
+    when 7 then 7
+    when 8 then 7
+    when 9..12 then 6
+    when 13..20 then 5
+    else
+      4
+  end
+  return max
+end
+
+def only_alphabetic?(text)
+  return /[^a-zA-Z]/.match(text).nil?
+end
+
+def positive_integer?(text)
+  return (Integer(text) > 0 rescue false)
+end
+
+puts "**** MASTERMIND ****"
+print "Use default settings? (Y/n) "
+response = gets.chomp
+
+if response.downcase == "n"
+  colors = get_colors()
+  code_length = get_code_length(2, max_length_for(colors.size))
+  max_guesses = get_max_guesses()
+  board = MastermindBoard.new(colors, code_length, max_guesses)
+else
+  board = MastermindBoard.new
+end
+
+puts "\n"
+puts "Ok, think you're a mastermind?"
 puts "The codemaker will choose a secret code, and the codebreaker must"
 puts "try to guess the secret code within *#{board.max_guesses}* tries!"
 puts "A + means there's a match in the right position;"
 puts "a - means there's match in the wrong position."
-puts "The code will be *#{board.spaces}* characters long,"
+puts "The code will be *#{board.code_length}* characters long,"
 puts "and made up of a combination of these letters:"
 puts "#{board.colors.join(", ")}"
 puts ""
@@ -207,11 +303,11 @@ puts ""
 print "Would you like to be the codemaker or codebreaker? (m/B) "
 choice = gets.chomp.downcase
 if choice == "m"
-  codemaker = HumanPlayer.new(board.colors, board.spaces)
-  codebreaker = ComputerPlayer.new(board.colors, board.spaces)
+  codemaker = HumanPlayer.new("Human", board.colors, board.code_length)
+  codebreaker = ComputerPlayer.new("Computer", board.colors, board.code_length)
 else
-  codemaker = ComputerPlayer.new(board.colors, board.spaces)
-  codebreaker = HumanPlayer.new(board.colors, board.spaces)
+  codemaker = ComputerPlayer.new("Computer", board.colors, board.code_length)
+  codebreaker = HumanPlayer.new("Human", board.colors, board.code_length)
 end
 
 valid_code = false
@@ -228,4 +324,4 @@ while !board.game_over?
   puts board
 end
 
-puts "Winner: #{board.victor}!"
+puts "#{board.points} points for #{codemaker.name}!"
